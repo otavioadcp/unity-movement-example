5348bce85192ba1ee1e75147906bd36eb73ed099
Otavio Pimentel <otavioadcp@gmail.com>
2025-08-05 20:57:04 -0400

    fix Game Over screen

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -3,21 +3,31 @@ using UnityEngine;
 public class Jump : MonoBehaviour
 {
 
+    public bool isAlive = true;
     public Rigidbody2D rig;
     public float jumpStr = 8;
+
+    public MainLogicScript mainLogic;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isAlive)
         {
             rig.linearVelocityY = jumpStr;
         }
+    }
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        mainLogic.gameOver();
+        isAlive = false;
     }
 }
--- Assets/Scripts/MainLogicScript.cs
+++ Assets/Scripts/MainLogicScript.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainLogicScript : MonoBehaviour
@@ -6,10 +7,24 @@ public class MainLogicScript : MonoBehaviour
     public Text scoreText;
     public int playerScore;
 
+    public GameObject gameOverScreen;
+
     [ContextMenu("Increase Score")]
     public void addScore()
     {
         playerScore++;
         scoreText.text = playerScore.ToString();
+    }
+
+    [ContextMenu("Restart Game")]
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    [ContextMenu("Game Over")]
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
     }
 }
